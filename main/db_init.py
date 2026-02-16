7f630ae0caf29b663277a57d44310e63c3ad67a8
lauranblanco <laurablanco12@javerianacali.edu.co>
2026-02-16 08:39:47 -0500

    Arreglar reinicio

--- main/db_init.py
+++ main/db_init.py
@@ -1,6 +1,8 @@
+import os
 import sqlite3
 
-DB_PATH = "database.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "database.db")
 
 def init_db():
     conn = sqlite3.connect(DB_PATH)
